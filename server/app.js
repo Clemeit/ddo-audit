2ae2b976317280c62e0a598376887c86a3811b67
Clemeit <tboulan@fastmail.com>
2022-01-25 00:24:30 -0700

    Removed NodeJS application routing

--- server/app.js
+++ server/app.js
@@ -1,40 +1,36 @@
 const compression = require("compression");
 const express = require("express");
 const cors = require("cors");
+const path = require("path");
 var admin = require("firebase-admin");
+
 const { initializeApp, applicationDefault } = require("firebase-admin/app");
 require("dotenv").config();
 
 const api = express();
-const app = express();
+// const app = express();
 
-const APP_PORT = process.env.APP_PORT;
+// const APP_PORT = process.env.APP_PORT;
 const API_PORT = process.env.API_PORT;
 
-app.use(express.static("../client")); // This is NOT good for production
-app.use(compression());
+// app.use(compression());
 
 api.use(cors());
 api.options("*", cors());
-app.use(cors());
-app.options("*", cors());
+// app.use(cors());
+// app.options("*", cors());
 
 // Major endpoints
 require("./population")(api);
-// require("./cron");
-
-// app.get(["/", "/*"], function (req, res, next) {
-// 	res.sendFile(path.join(__dirname, "../client", "index.html"));
-// });
 
 // Firebase
 initializeApp({
 	credential: applicationDefault(),
 });
 
-app.listen(APP_PORT, () => {
-	console.log(`Front-end listening on ${APP_PORT}`);
-});
+// app.listen(APP_PORT, () => {
+// 	console.log(`Front-end listening on ${APP_PORT}`);
+// });
 
 api.listen(API_PORT, () => {
 	console.log(`API listening on ${API_PORT}`);
