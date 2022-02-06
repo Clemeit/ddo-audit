be7586e1a46dd8769824c4cf464998b21baafda8
Clemeit <tboulan@fastmail.com>
2022-02-06 13:35:36 -0700

    Added additional endpoints

--- server/app.js
+++ server/app.js
@@ -2,38 +2,30 @@ const compression = require("compression");
 const express = require("express");
 const cors = require("cors");
 const path = require("path");
-var admin = require("firebase-admin");
+let admin = require("firebase-admin");
+let mysql = require("mysql2");
 
 const { initializeApp, applicationDefault } = require("firebase-admin/app");
 require("dotenv").config();
 
 const api = express();
-// const app = express();
-
-// const APP_PORT = process.env.APP_PORT;
 const API_PORT = process.env.API_PORT;
-
-// app.use(compression());
 
 api.use(cors());
 api.options("*", cors());
-// app.use(cors());
-// app.options("*", cors());
 
 // Major endpoints
 require("./Endpoints/Population")(api);
 require("./Endpoints/Demographics")(api);
+require("./Endpoints/Groups")(api);
+require("./Endpoints/Players")(api);
+require("./Endpoints/MessageService")(api);
+require("./Endpoints/GameStatus")(api);
 
 // Firebase
 initializeApp({
 	credential: applicationDefault(),
 });
-
-// app.listen(APP_PORT, () => {
-// 	console.log(`Front-end listening on ${APP_PORT}`);
-// });
-
-// require("./ReportWorker");
 
 api.listen(API_PORT, () => {
 	console.log(`API listening on ${API_PORT}`);
