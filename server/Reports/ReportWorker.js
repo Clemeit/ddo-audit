831b01bfa5f44671ef93f5bcc19deb31ade47bc8
Clemeit <tboulan@fastmail.com>
2022-02-06 13:36:00 -0700

    Fixed caching bug

--- server/Reports/ReportWorker.js
+++ server/Reports/ReportWorker.js
@@ -14,6 +14,8 @@ const { runDailyDistribution } = require("./Population/DailyDistribution");
 const { runHourlyDistribution } = require("./Population/HourlyDistribution");
 const { runServerDistribution } = require("./Population/ServerDistribution");
 
+const { runServerStatusReport } = require("./Game/ServerStatus");
+
 var mysql = require("mysql2");
 var con = mysql.createConnection({
 	host: process.env.DB_HOST,
@@ -28,39 +30,6 @@ function GetDateString(datetime) {
 	}-${datetime.getUTCDate()} 00-00-00`;
 }
 
-function IsPlayerActive(
-	lastseen,
-	lastactive,
-	lastmovement,
-	lastlevelup,
-	totallevel
-) {
-	let MOVEMENT_DAY_THRESHOLD = 5;
-	let QUESTING_DAY_THRESHOLD = 10;
-	let LEVELUP_DAY_THRESHOLD = 20;
-
-	let seen = new Date(lastseen + "Z").getTime();
-	let active = lastactive == null ? 0 : new Date(lastactive + "Z").getTime();
-	let movement =
-		lastmovement == null ? 0 : new Date(lastmovement + "Z").getTime();
-	let levelup =
-		lastlevelup == null ? 0 : new Date(lastlevelup + "Z").getTime();
-
-	let isactive = true;
-
-	if (seen - movement > 1000 * 60 * 60 * 24 * MOVEMENT_DAY_THRESHOLD)
-		isactive = false; // No movement
-	if (seen - active > 1000 * 60 * 60 * 24 * QUESTING_DAY_THRESHOLD)
-		isactive = false; // No questing
-	if (
-		totallevel < 30 &&
-		seen - levelup > 1000 * 60 * 60 * 24 * LEVELUP_DAY_THRESHOLD
-	)
-		isactive = false; // No level-ups (level 30s excluded)
-
-	return isactive;
-}
-
 con.connect((err) => {
 	if (err) throw err;
 	console.log("Connected to database");
@@ -68,6 +37,7 @@ con.connect((err) => {
 	// Get class data:
 	let classes = [];
 	function getClassData() {
+		classes.length = 0;
 		let classquery = "SELECT * FROM `classes` ORDER BY `classes`.`name` ASC;";
 		con.query(classquery, (err, result, fields) => {
 			if (err) throw err;
@@ -87,6 +57,7 @@ con.connect((err) => {
 	// Get race data:
 	let races = [];
 	function getRaceData() {
+		races.length = 0;
 		let racequery = "SELECT * FROM `races` ORDER BY `races`.`name` ASC;";
 		con.query(racequery, (err, result, fields) => {
 			if (err) throw err;
@@ -114,6 +85,7 @@ con.connect((err) => {
 	let players = [];
 	function getPlayerData(days) {
 		return new Promise(async (resolve, reject) => {
+			players.length = 0;
 			let query =
 				"SELECT * FROM `players` WHERE `lastseen` >= '" +
 				GetDateString(
@@ -139,12 +111,11 @@ con.connect((err) => {
 	let population = [];
 	function getPopulationData(days) {
 		return new Promise(async (resolve, reject) => {
+			population.length = 0;
 			let query =
 				"SELECT * FROM `population` WHERE `datetime` >= '" +
 				GetDateString(
-					new Date(
-						new Date(new Date().toDateString()) - 60000 * 60 * 24 * days
-					)
+					new Date(new Date().getTime() - 60000 * 60 * 24 * days)
 				) +
 				"' ORDER BY `population`.`datetime` ASC;";
 			con.query(query, (err, result, fields) => {
@@ -162,11 +133,6 @@ con.connect((err) => {
 			});
 		});
 	}
-
-	// Select which reports to run based on the time and day
-	let dayofweek = new Date().getUTCDay();
-	let hourofday = new Date().getUTCHours();
-	let minuteofhour = new Date().getUTCMinutes();
 
 	// Every week
 	cron.schedule("0 * * * 0", () => {
@@ -193,9 +159,12 @@ con.connect((err) => {
 		getClassData();
 		getRaceData();
 		getPlayerData((days = 91)).then(() => {
-			runClassDistribution(players, classes);
-			runRaceDistribution(players, races);
-			runLevelDistribution(players);
+			runClassDistribution(players, classes, "normal");
+			runRaceDistribution(players, races, "normal");
+			runLevelDistribution(players, "normal");
+			runClassDistribution(players, classes, "banks");
+			runRaceDistribution(players, races, "banks");
+			runLevelDistribution(players, "banks");
 		});
 	});
 
@@ -203,7 +172,7 @@ con.connect((err) => {
 	cron.schedule("0 0-22 * * *", () => {});
 
 	// Every 5 minutes
-	cron.schedule("0-55/5 * * * *", () => {
+	cron.schedule("1-56/5 * * * *", () => {
 		getPopulationData(1).then(() => {
 			runDayReport(population);
 		});
@@ -211,47 +180,6 @@ con.connect((err) => {
 
 	// Every minute
 	cron.schedule("* * * * *", () => {
-		// runServerStatusReport();
+		runServerStatusReport();
 	});
-
-	// if (dayofweek * hourofday * minuteofhour === 0) {
-	// 	// Run once per week
-	// 	console.log("Running weekly report...");
-	// 	getPopulationData(365).then(() => {
-	// 		runAnnualReport(population);
-	// 		runQuarterReport(population);
-	// 		runWeekReport(population);
-	// 		runDailyDistribution(population);
-	// 		runHourlyDistribution(population);
-	// 		runServerDistribution(population);
-	// 	});
-	// } else if (hourofday * minuteofhour === 0) {
-	// 	// Run once per day
-	// 	console.log("Running daily report...");
-	// 	getPopulationData(365).then(() => {
-	// 		runQuarterReport(population);
-	// 		runWeekReport(population);
-	// 		runDailyDistribution(population);
-	// 		runHourlyDistribution(population);
-	// 		runServerDistribution(population);
-	// 	});
-
-	// 	getClassData();
-	// 	getRaceData();
-	// 	getPlayerData((days = 91)).then(() => {
-	// 		runClassDistribution(players, classes);
-	// 		runRaceDistribution(players, races);
-	// 		runLevelDistribution(players);
-	// 	});
-	// } else if (minuteofhour === 0) {
-	// 	// Run once per hour
-	// } else if (minuteofhour % 5 === 0) {
-	// 	// Run every 5 minutes
-	// 	getPopulationData(1).then(() => {
-	// 		runDayReport(population);
-	// 	});
-	// } else {
-	// 	// Run every time (1 minute)
-	// 	// runServerStatusReport();
-	// }
 });
