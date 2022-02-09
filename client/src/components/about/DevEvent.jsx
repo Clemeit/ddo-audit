cbd54fca106d6d98ee373b9160921447af34cef5
Clemeit <tboulan@fastmail.com>
2022-02-09 12:32:02 -0700

    Added color option

--- client/src/components/about/DevEvent.jsx
+++ client/src/components/about/DevEvent.jsx
@@ -3,7 +3,12 @@ import React from "react";
 const DevEvent = (props) => {
     return (
         <div>
-            <h5 className="dev-event-title">{props.title}</h5>
+            <h5
+                className="dev-event-title"
+                style={{ color: props.color || "" }}
+            >
+                {props.title}
+            </h5>
             <ul className="dev-event-content">
                 {props.events.map((event, i) => (
                     <li key={i}>{event}</li>
