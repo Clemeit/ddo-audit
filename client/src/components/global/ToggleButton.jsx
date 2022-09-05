b9fcb7b9818e5c6e562ba2b6e451d9425ce2b5f9
Clemeit <tboulan@fastmail.com>
2022-09-05 15:09:01 -0600

    Better toggle buttons

--- client/src/components/global/ToggleButton.jsx
+++ client/src/components/global/ToggleButton.jsx
@@ -2,32 +2,39 @@ import React from "react";
 
 const ToggleButton = (props) => {
     return (
-        <div
-            style={{ display: "flex", gap: "0px", paddingBottom: "5px" }}
-            className={props.className}
-        >
-            <button
-                className={`secondary-button should-invert${
-                    props.isA ? " selected" : ""
-                }`}
-                style={{ fontSize: "1rem", borderRadius: "3px 0px 0px 3px" }}
-                onClick={() => {
-                    props.doA();
-                }}
-            >
-                {props.textA}
-            </button>
-            <button
-                className={`secondary-button should-invert${
-                    props.isB ? " selected" : ""
-                }`}
-                style={{ fontSize: "1rem", borderRadius: "0px 3px 3px 0px" }}
-                onClick={() => {
-                    props.doB();
-                }}
-            >
-                {props.textB}
-            </button>
+        <div className={`toggle-button-container ${props.className}`}>
+            <div className="toggle-button-inner">
+                <button
+                    className={`toggle-button left${
+                        props.isA ? " selected" : ""
+                    }`}
+                    style={{
+                        fontSize: "1rem",
+                        borderRadius: "3px 0px 0px 3px",
+                    }}
+                    onClick={() => {
+                        props.doA();
+                    }}
+                >
+                    <div />
+                    <span>{props.textA}</span>
+                </button>
+                <button
+                    className={`toggle-button right${
+                        props.isB ? " selected" : ""
+                    }`}
+                    style={{
+                        fontSize: "1rem",
+                        borderRadius: "0px 3px 3px 0px",
+                    }}
+                    onClick={() => {
+                        props.doB();
+                    }}
+                >
+                    <div />
+                    <span>{props.textB}</span>
+                </button>
+            </div>
         </div>
     );
 };
